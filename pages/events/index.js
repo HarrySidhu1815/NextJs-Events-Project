40c81cea4c41432fa64f250f5104f6fb2ecdc6d7
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2024-08-16 07:56:53 -0700

    programmatic navigation added

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,13 +2,21 @@ import React from 'react'
 import { getAllEvents } from '../../dummy_data'
 import EventList from '../../components/events/event-list'
 import EventSearch from '../../components/event-search/event-search'
+import { useRouter } from 'next/router'
 
 export default function EventPage() {
   const events = getAllEvents()
+  const router = useRouter()
+
+  function handleFilteredData(selectedYear, selectedMonth){
+    const fullPath = `/events/${selectedYear}/${selectedMonth}`
+
+    router.push(fullPath)
+  }
 
   return (
     <>
-      <EventSearch />
+      <EventSearch onSearch={handleFilteredData}/>
       <EventList items={events} />
     </>
   )
