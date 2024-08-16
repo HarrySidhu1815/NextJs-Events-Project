6c300328cd1fcb0ccc2e145acd8e3820490409be
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2024-08-16 07:34:40 -0700

    nav bar and all events page added

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { getAllEvents } from '../../dummy_data'
+import EventList from '../../components/events/event-list'
 
 export default function EventPage() {
+  const events = getAllEvents()
 
   return (
     <div>
-      <ul></ul>
+      <EventList items={events} />
     </div>
   )
 }
